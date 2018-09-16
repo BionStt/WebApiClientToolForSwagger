5c2b48ef1d4dc5227045ce40d7e6933ee4957c68
fanpan26 <fanpan26@126.com>
2018-09-16 15:19:01 +0800

    重新调整项目结构

--- Tool/WebApiClient.Tool.Console/Program.cs
+++ Tool/WebApiClient.Tool.Console/Program.cs
@@ -7,15 +7,15 @@ namespace WebApiClient.Tool.Console
 {
     class Program
     {
+
+        static string swaggerJsonUrl = "http://localhost:5000/swagger/v1/swagger.json";
+
+
         static void Main(string[] args)
         {
             SwaggerToWebApiClientGenerator codeGenerator = new SwaggerToWebApiClientGenerator();
-            codeGenerator.Start("http://localhost:5000/swagger/v1/swagger.json").Wait();
+            codeGenerator.Start(swaggerJsonUrl).Wait();
             SysConsole.WriteLine("finished...");
-
-            //Test.NameSpace.ISwaggerApi swaggerApi = HttpApiClient.Create<Test.NameSpace.ISwaggerApi >("http://localhost:5000");
-            //var result = swaggerApi.ApiValuesById(1).GetAwaiter().GetResult();
-            //SysConsole.WriteLine("获取到的结果：" + result.result + ".score:" + result.score);
             SysConsole.Read();
         }
     }
